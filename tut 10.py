2a9f9d45ff1847fe61abd97ae73653f6aa7c0a9d
SANYAM1996 <45790664+SANYAM1996@users.noreply.github.com>
2018-12-30 11:01:51 +0530

    having error while running this dictionary

---
+++ tut 10.py
@@ -0,0 +1,14 @@
+d2 = {"Outer Space":"Outer Space means all space other than the earth. Outer space is vast",
+      "Milky Way":"Milky Way is our home galaxy.The Milky Way is faint band of light crossing the night sky.It consists of stars in the Plane of our Galaxy.Milky Way is a spiral Galaxy about 100,000 light years in diameter,containing at least 100 billion stars, like our sun",
+
+    "The Sun":"The Sun is the star at the center of the solar system.The Sun is about 4.7 billion years old and is composed about 70% hydrogen and 30% helium with other elements making up less than 1%. The Sun is at the distance of about 32,000 light years from the center of the Galaxy.The diameter of the Sun is 1,392,000km.",
+
+      "Meteors":"Meteor,the flash of light in the sky, popularly known as falling star or shooting star is caused by a particles of dust. Meteoroid is a piece of rock or metal like substance from space that reaches the earth’s surface or the surface of other celestial body. ",
+
+      "constellation":"A constellation is a group of bright stars that form shapes or 'pictures' in the sky.The Southern Cross is a constellation in the Southern hemisphere that points towards the South Pole. The Big Dipper is a constellation in the Northern hemisphere that is in the shape of a ladle or a large spoon",
+
+      "space probe":"A space probe is a type of space craft that does not have a person inside.Space probes can be sent to far away distances for long periods of time to gather information about different areas in space",
+      
+
+
+print(d2["Outer Space"])
